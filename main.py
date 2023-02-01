8c2c7f142ba5c43f4c70e39662ce842c47ccbb3c
Nilson Perboni <nilson.a.p.neto@gmail.com>
2023-02-01 11:09:45 +0000

    Finished all challenge tasks

--- main.py
+++ main.py
@@ -51,9 +51,10 @@ Considerar casos onde período ou começou e não terminou, ou terminou e não c
 """
 
 import os
+import sys
 import awswrangler as wr
 
-from geopy.distance import geodesic
+from datetime import datetime
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import to_timestamp, udf, struct
 from pyspark.sql.types import FloatType
@@ -61,6 +62,8 @@ from sqlalchemy import create_engine
 
 raw_data_bucket = "de-tech-assessment-2022"
 raw_data_prefix = "data"
+dw_data_bucket = "de-tech-assessment-2022-nilson"
+dw_data_prefix = "data"
 timestamp_format = "yyyy-MM-dd'T'HH:mm:ss.SSSX"
 
 spark = SparkSession.builder.appName("Loka Application").getOrCreate()
@@ -92,11 +95,6 @@ def download_only_new_files(file_list: list[str]) -> str:
     return folder
 
 
-@udf(returnType=FloatType())
-def geodesic_udf(a, b):
-    return geodesic(a, b).km
-
-
 def get_events_from_date(event_date: str = "") -> str:
     """
     Downloads events of a certain date to local folder.
@@ -107,6 +105,24 @@ def get_events_from_date(event_date: str = "") -> str:
     event_list = get_s3_paths(event_date)
     events_folder = download_only_new_files(event_list)
     return events_folder
+
+
+def save_files_to_datalake(events_folder: str):
+    all_files = os.listdir(events_folder)
+    for event_file in all_files:
+        local_path = f"{os.path.normpath(events_folder)}/{event_file}"
+        s3_path = (
+            f"s3://{dw_data_bucket}/{dw_data_prefix}/{datetime.today()}/{event_file}"
+        )
+        wr.s3.upload(local_file=local_path, path=s3_path)
+
+
+def delete_tmp_folder(events_folder: str):
+    try:
+        os.rmdir(events_folder)
+        print(f"Deleted temporary folder {events_folder}")
+    except Exception as e:
+        print(f"Error while deleting  {events_folder}: {e}")
 
 
 def read_events_into_dataframe(events_folder: str) -> DataFrame:
@@ -136,24 +152,7 @@ def fix_dataframe_dates(df: DataFrame) -> DataFrame:
     return df
 
 
-def save_dataframe_to_postgres(df: DataFrame, table_name: str):
-    pdf = df.toPandas()
-    pdf.to_sql(table_name, engine, index=False, if_exists="append")
-
-
-# read event
-# Possible problems identified:
-# * Events that *arrive* after processing the old data
-# * Would have to
-
-
-if __name__ == "__main__":
-    events_folder = get_events_from_date("2019-06-01")
-    # Store data on S3
-    # get_events_from_date()
-    df = read_events_into_dataframe(events_folder)
-    df = fix_dataframe_dates(df)
-
+def get_vehicle_and_period_dfs_from_df(df: DataFrame) -> tuple[DataFrame, DataFrame]:
     df_vehicle = (
         df.where(df.on == "vehicle")
         .withColumn("data_id", df.data.id)
@@ -169,7 +168,54 @@ if __name__ == "__main__":
         .withColumn("date_finish", df.data.date_finish)
         .drop(df.data)
     )
-    save_dataframe_to_postgres(df_vehicle, "vehicle")
+
+    return df_vehicle, df_operating_period
+
+
+def get_vehicles_events_during_operating_hours(
+    df_vehicle: DataFrame, df_operating_period: DataFrame
+) -> DataFrame:
+
+    # Filtering could be done by inner join, but is not the most elegant solution.
+    # So, filtering will be done in a code format, making its purpose clearer.
+    final_df_vehicle = spark.createDataFrame(
+        data=spark.sparkContext.emptyRDD(), schema=df_vehicle.schema
+    )
+    df_hours = df_operating_period.filter(df_operating_period.event == "create")
+    for i in df_hours.collect():
+        period_start = i["date_start"]
+        period_finish = i["date_finish"]
+        final_df_vehicle = final_df_vehicle.unionAll(
+            df_vehicle.filter(
+                df_vehicle.location_at.between(period_start, period_finish)
+            )
+        )
+    return final_df_vehicle
+
+
+def save_dataframe_to_postgres(df: DataFrame, table_name: str):
+    pdf = df.toPandas()
+    pdf.to_sql(table_name, engine, index=False, if_exists="append")
+
+
+def get_events_process_and_save_to_dw(events_date: str):
+    events_folder = get_events_from_date(events_date)
+    save_files_to_datalake(events_folder)
+    df = read_events_into_dataframe(events_folder)
+    df = fix_dataframe_dates(df)
+    df_vehicle, df_operating_period = get_vehicle_and_period_dfs_from_df(df)
+    final_df_vehicle = get_vehicles_events_during_operating_hours(
+        df_vehicle, df_operating_period
+    )
+    save_dataframe_to_postgres(final_df_vehicle, "vehicle")
     save_dataframe_to_postgres(df_operating_period, "operating_period")
-    # Displays the content of the DataFrame to stdout
-    # df = df.withColumn("Lengths/m", geodesic_udf(F.array("B", "A"), F.array("D", "C")))
+    delete_tmp_folder(events_folder)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        raise Exception("No date was informed")
+    else:
+        events_date = sys.argv[1]
+
+    get_events_process_and_save_to_dw(events_date)
